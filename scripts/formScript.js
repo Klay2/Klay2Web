0cbead825ab1e41d3b5f5a48edfdf8b991fa4492
Klay2 <klaykrueger@wisc.edu>
2016-06-03 21:10:14 -0500

    experimenting some more

---
+++ scripts/formScript.js
@@ -0,0 +1,3 @@
+function handleForm1(){
+  
+}
--- scripts/main_index.js
+++ scripts/main_index.js
@@ -1,8 +1,5 @@
+
 function test1(selector,words){
-
-
-
-
     document.querySelector(selector+"").textContent = words;
     return;
 }
@@ -11,14 +8,29 @@ function alertTest(){
   alert("ClicketyClickety");
 }
 
+function newDiv(){
+  var newDiv = document.createElement('div');
+  newDiv.style.width = "200px";
+  newDiv.style.height = "200px";
+  newDiv.style.background = "red";
+  newDiv.style.color = "white";
+  newDiv.innerHTML="<h2>ThingName</h2><p>Preference: 100<button>+</button><button>-</button><br>Effect: 25<button>+</button><button>-</button></p>";
+
+  /*
+  newDiv.innerHTML="hi";*/
+  document.body.appendChild(newDiv);
+  return;
+}
+/*
 function getText(){
   var textIn = prompt('enter element');
   localStorage.setItem('input',textIn);
   var textIn2 = prompt('enter text');
   localStorage.setItem('input2',textIn2);
   test1(textIn,textIn2);
+  return;
 }
-
+*/
 
 document.querySelector("#main_header_title_text").onclick = function(){
   if(document.querySelector("#main_header_title_text").textContent === "KlaymoJones"){
@@ -29,6 +41,8 @@ document.querySelector("#main_header_title_text").onclick = function(){
   }
 };
 
-document.querySelector('button').onclick = function(){
-  getText();
+
+document.querySelector("#newDivButton").onclick = function(){
+  alert("getting this far");
+  newDiv();
 };
